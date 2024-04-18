365a7ad3fedc15f6adb39183faaea0be911acabd
PyDayCochabamba <pydaycbba@gmail.com>
2024-04-17 21:51:08 -0400

    first commit

---
+++ pyday/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-=g%tklouh52%+2r_+dbul3w(2r!r38ys@rse795e9h&su_l(+t"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
